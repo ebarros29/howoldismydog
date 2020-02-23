3ac343d95114126e607584e8de4242fcd14c50e5
ebarros29 <emerson.emmanuel@gmail.com>
2020-02-23 01:30:23 -0300

    creating new condition called error

--- dog_human_age.py
+++ dog_human_age.py
@@ -8,7 +8,7 @@ db = client['dogs_db']
 coll_raca = db['racas']
 
 def get_human_age(dog_breed, dog_age, dog_size):
-
+    x=[]
     id_c = int(dog_age)
 
     human_age = [0, 15, 24, 28, 32, 36, 
@@ -18,17 +18,20 @@ def get_human_age(dog_breed, dog_age, dog_size):
                 [64, 69, 77], [68, 74, 82],
                 [72, 78, 88], [76, 83 ,93],
                 [80, 87, 120]]
-    
+
     mydict=coll_raca.find({'$text': {'$search': "\"%s\"" % (dog_breed)}})
-    
+
     for x in mydict:
-        print (x)
+        print(x)
+
+    if len(x) == 0:
+        return "error"
 
     if dog_size == "":
         dog_size = x['porte']
     else:
         dog_size = dog_size
-    
+
     if id_c <= 5:
         return x['raca'], human_age[id_c], x['origem'], x['personalidade']
 
@@ -42,4 +45,7 @@ def get_human_age(dog_breed, dog_age, dog_size):
     else:
         return "Error"
 
-#print (get_human_age("bull ter", 8, "Pequeno"))
+#response = get_human_age("aki", 5, "")
+
+#print(response)
+
