38238ea77c9a89e5d608509a5740c650ab26f61b
Emerson Barros <emerson.emmanuel@gmail.com>
2020-06-21 23:01:01 -0300

    imported to mongo from dogs_db.json

--- services/mongo-seed/import.sh
+++ services/mongo-seed/import.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-mongoimport --host mongo --db dogs_db --collection racas --type json --file /mongo-seed/dogs_db_2.json --jsonArray
+mongoimport --host mongo --db dogs_db --collection racas --type json --file /mongo-seed/dogs_db.json --jsonArray
 
 mongo mongo:27017/dogs_db /mongo-seed/create_index.js
